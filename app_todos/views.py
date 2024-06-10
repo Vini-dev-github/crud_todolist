0f1bd35edaf9a6c7e43c91ff01620f33acdc5158
Vini <viniciuslima.profissional@gmail.com>
2024-06-10 16:37:25 -0300

    add update

--- app_todos/views.py
+++ app_todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Todo
 
@@ -11,3 +11,9 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = ["title","deadline"]
     success_url = reverse_lazy("todo_list")
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+    
--- project_todo/urls.py
+++ project_todo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from app_todos.views import TodoListView, TodoCreateView
+from app_todos.views import TodoListView, TodoCreateView, TodoUpdate
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('', TodoListView.as_view(), name="todo_list"),
     path('create/', TodoCreateView.as_view(), name="todo_create"),
+    path('update/<int:pk>', TodoUpdate.as_view(), name="todo_update")
 ]
